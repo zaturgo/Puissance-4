33440dbedb1a90c1f8533bf702962679c0015750
Hugo BLANCHARD <hugo@wigo-media.com>
2019-10-23 13:39:50 +0200

    game

--- src/Controllers/Game.java
+++ src/Controllers/Game.java
@@ -13,7 +13,6 @@ public class Game {
     public Game() {
         _grid = new Grid();
     }
-
     public void start(){
         boolean game = true;
         Human p1 = new Human("Jean", 1);
@@ -21,12 +20,23 @@ public class Game {
         ArrayList<Player> tabPlayers= new ArrayList<>();
         tabPlayers.add(p1);
         tabPlayers.add(p2);
-
+    //game loop, stops when game == false;
         while(game){
             for (int i = 0; i<tabPlayers.size(); i++){
-                System.out.println("Tour du premier joueur");
-                _grid.placeToken(p1.get_id(),p1.getAction());
-                _grid.checkWin();
+                System.out.println("Tour du joueur "+i);
+                try{
+                    //add token
+                    _grid.placeToken(tabPlayers.get(i).get_id(),tabPlayers.get(i).getAction());
+                }catch(Exception e){
+                    System.out.println("Placement invalide");
+                }
+                //check if 4 aligned
+                int result = _grid.checkWin();
+                if (result!=0){
+                    game= true;
+                    System.out.println("Joueur gagnant:"+result);
+                }
+                _grid.debugGrid();
             }
         }
     }
--- src/Models/Player.java
+++ src/Models/Player.java
@@ -1,9 +1,25 @@
 package Models;
 
+import java.util.Scanner;
+
 public class Player {
     private Integer _id;
     public Integer getAction(){
-        return 3;//Integer between 0 and 6
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("Colonne visée:");
+            String str = sc.nextLine();
+            try{
+                int col = Integer.parseInt(str);
+                if (col<=6 && col>=0){
+                    return col;
+                }else{
+                    System.out.println("Entier entre 0 et 6 uniquement");
+                }
+            }catch(Exception e){
+                System.out.println("mauvais format");
+            }
+        }
     }
 
     public Player(Integer _id) {
